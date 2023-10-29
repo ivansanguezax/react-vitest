1ab625659709bc525f6149b77238cd84d3c9ae7a
ivansanguezax <ivansanguezax@gmail.com>
2023-10-28 21:36:36 -0400

    [003-testComponent]

--- src/components/Accordion.test.tsx
+++ src/components/Accordion.test.tsx
@@ -1,5 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Accordion from "./Accordion";
+
 describe("Accordion", () => {
-  test('should add two numbers', () => {
-    expect(1 + 1).toBe(2)
-  })
+  test("should add two numbers", () => {
+    render(
+      <Accordion title="hola">
+        <h3>My content</h3>
+        <p>Some content</p>
+      </Accordion>
+    );
+    expect(screen.getByText('hola')).toBeDefined();
+  });
 });
